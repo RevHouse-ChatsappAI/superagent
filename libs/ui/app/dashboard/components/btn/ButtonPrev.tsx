1c918242594d38415f9b51bfc4917a56692fec84
GonzaloArray <gonzalo.arrayaran@gmail.com>
2024-02-29 01:03:21 -0300

    Fix Add Agent Created

--- libs/ui/app/dashboard/components/btn/ButtonPrev.tsx
+++ libs/ui/app/dashboard/components/btn/ButtonPrev.tsx
@@ -9,7 +9,7 @@ export const ButtonPrev: React.FC<Props> = ({ title, prevStep }) => {
   return (
     <button
       onClick={prevStep}
-      className="rounded-md border border-transparent bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+      className="rounded-md border border-transparent px-4 py-2 text-sm font-medium transition-colors duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:border-gray-600 dark:bg-gray-700 dark:text-gray-300 dark:hover:bg-gray-600 dark:focus:ring-offset-gray-800"
     >
       {title}
     </button>
--- libs/ui/app/dashboard/components/step/StepThree.tsx
+++ libs/ui/app/dashboard/components/step/StepThree.tsx
@@ -4,10 +4,10 @@ import { useAsync } from "react-use"
 
 import { Profile } from "@/types/profile"
 import { Api } from "@/lib/api"
+import { Spinner } from "@/components/ui/spinner"
 import { useChatwoot } from "@/app/context/ChatwootContext"
 
 import { ButtonPrev } from "../btn/ButtonPrev"
-import { Spinner } from "@/components/ui/spinner"
 
 interface StepOneProps {
   nextStep: () => void
@@ -16,7 +16,12 @@ interface StepOneProps {
   profile: Profile
 }
 
-const StepThree = ({ nextStep, prevStep, profile, btnPrevActive = true }: StepOneProps) => {
+const StepThree = ({
+  nextStep,
+  prevStep,
+  profile,
+  btnPrevActive = true,
+}: StepOneProps) => {
   //State
   const [loading, setLoading] = useState(false)
   const [form, setForm] = useState({
@@ -30,12 +35,23 @@ const StepThree = ({ nextStep, prevStep, profile, btnPrevActive = true }: StepOn
   })
 
   //Context
-  const { handleAgentApi } = useChatwoot()
+  const { apiAgent, handleAgentApi } = useChatwoot()
 
   //API
   const api = useMemo(() => new Api(profile.api_key), [profile.api_key])
 
   //Function
+  const { value: agents, error: agentsError } = useAsync(async () => {
+    const response = await api.getAgents({
+      skip: 0,
+      take: 10,
+    })
+    if (response.error) {
+      throw new Error(response.error)
+    }
+    return response.data
+  }, [])
+
   const handleAddUserChatwoot = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     setLoading(true)
@@ -71,61 +87,127 @@ const StepThree = ({ nextStep, prevStep, profile, btnPrevActive = true }: StepOn
     }
   }
 
+  const [modalActiveAgent, setModalActiveAgent] = useState(true)
+
   return (
-    <div className="flex flex-1 flex-col">
-      <h2 className="mb-4 text-sm text-gray-700 dark:text-gray-500">
-        Creación de Agente ChatsAppAI
-      </h2>
-      <form onSubmit={handleAddUserChatwoot} className="flex flex-1 flex-col justify-between">
-        <div className="flex flex-col gap-4">
-          <label className="flex w-full flex-col gap-1">
-            <p className="text-sm">Nombre del agente</p>
-            <input
-              type="text"
-              name="name"
-              value={form.name}
-              onChange={(e) =>
-                setForm({
-                  ...form,
-                  name: e.target.value,
-                })
-              }
-              className="w-full rounded-lg border border-gray-300 bg-transparent p-2 text-sm text-gray-900 placeholder:text-gray-500 dark:border-gray-600 dark:text-gray-100 dark:placeholder:text-gray-400"
-              placeholder="Ej: Asistente Carlos Carniceria"
-              required
-            />
-          </label>
-          <label className="flex w-full flex-col gap-1">
-            <p className="text-sm">Description</p>
-            <input
-              type="text"
-              name="description"
-              value={form.description}
-              onChange={(e) =>
-                setForm({
-                  ...form,
-                  description: e.target.value,
-                })
-              }
-              className="w-full rounded-lg border border-gray-300 bg-transparent p-2 text-sm text-gray-900 placeholder:text-gray-500 dark:border-gray-600 dark:text-gray-100 dark:placeholder:text-gray-400"
-              placeholder="Ej: Agente de soporte técnico para clientes VIP"
-              required
-            />
-          </label>
-        </div>
-        <div className={`mt-3 flex items-center ${btnPrevActive ? 'justify-between' : 'justify-end'}`}>
-          {
-            btnPrevActive && <ButtonPrev title="Previo" prevStep={prevStep} />
-          }
-          <button
-            type="submit"
-            className="rounded bg-blue-500 px-4 py-2 text-sm text-white transition-all hover:bg-blue-400"
-            disabled={loading}
+    <div className="flex flex-1 flex-col dark:bg-gray-800">
+      <button
+        type="button"
+        className="mb-4 rounded bg-gray-300 px-4 py-2 text-sm text-black transition-all hover:bg-gray-200 dark:bg-gray-700 dark:text-white dark:hover:bg-gray-600"
+        onClick={() => setModalActiveAgent(!modalActiveAgent)}
+      >
+        {modalActiveAgent ? "Ya tengo un agente" : "Crear un nuevo agente"}
+      </button>
+      {modalActiveAgent ? (
+        <>
+          <h2 className="mb-4 text-sm text-gray-700 dark:text-gray-300">
+            Creación de Agente ChatsAppAI
+          </h2>
+          <form
+            onSubmit={handleAddUserChatwoot}
+            className={`flex flex-1 flex-col justify-between ${apiAgent ? 'pointer-events-none opacity-50' : ''}`}
           >
-            {loading ? <Spinner/> : "Crear Agent SuperAgent"}
-          </button>
+            <div className="flex flex-col gap-4">
+              <label className="flex w-full flex-col gap-1">
+                <p className="text-sm text-gray-900 dark:text-gray-300">Nombre del agente</p>
+                <input
+                  type="text"
+                  name="name"
+                  value={form.name}
+                  onChange={(e) =>
+                    setForm({
+                      ...form,
+                      name: e.target.value,
+                    })
+                  }
+                  className="w-full rounded-lg border border-gray-300 bg-transparent p-2 text-sm text-gray-900 placeholder:text-gray-500 dark:border-gray-600 dark:bg-gray-700 dark:text-gray-300 dark:placeholder:text-gray-400"
+                  placeholder="Ej: Asistente Carlos Carniceria"
+                  required
+                  disabled={!!apiAgent}
+                />
+              </label>
+              <label className="flex w-full flex-col gap-1">
+                <p className="text-sm text-gray-900 dark:text-gray-300">Description</p>
+                <input
+                  type="text"
+                  name="description"
+                  value={form.description}
+                  onChange={(e) =>
+                    setForm({
+                      ...form,
+                      description: e.target.value,
+                    })
+                  }
+                  className="w-full rounded-lg border border-gray-300 bg-transparent p-2 text-sm text-gray-900 placeholder:text-gray-500 dark:border-gray-600 dark:bg-gray-700 dark:text-gray-300 dark:placeholder:text-gray-400"
+                  placeholder="Ej: Agente de soporte técnico para clientes VIP"
+                  required
+                  disabled={!!apiAgent}
+                />
+              </label>
+            </div>
+            <div
+              className={`mt-5 flex items-center ${
+                btnPrevActive ? "justify-between" : "justify-end"
+              }`}
+            >
+              {btnPrevActive && (
+                <ButtonPrev title="Previo" prevStep={prevStep} />
+              )}
+              <button
+                type="submit"
+                className={`rounded bg-blue-500 px-4 py-2 text-sm text-white transition-all hover:bg-blue-400 ${loading || !!apiAgent ? 'cursor-not-allowed' : ''} dark:hover:bg-blue-600`}
+                disabled={loading || !!apiAgent}
+              >
+                {loading ? <Spinner /> : "Crear Agent SuperAgent"}
+              </button>
+            </div>
+          </form>
+        </>
+      ) : (
+        <div>
+          <h2 className="mb-4 text-sm text-gray-700 dark:text-gray-300">
+            Selecciona un agente:
+          </h2>
+
+          <div className="grid grid-cols-3 gap-4">
+            {agents?.map(
+              (agent: { id: string; name: string; description: string }) => (
+                <div
+                  key={agent.id}
+                  className={`flex h-20 cursor-pointer flex-col items-center justify-center rounded-lg bg-slate-300 p-4 shadow transition-opacity duration-300 ease-in-out dark:bg-gray-700 ${
+                    apiAgent && apiAgent !== agent.id
+                      ? "opacity-50"
+                      : "opacity-100"
+                  }`}
+                  onClick={() => {
+                    const newApiAgent = apiAgent === agent.id ? "" : agent.id;
+                    handleAgentApi(newApiAgent);
+                  }}
+                >
+                  <p
+                    className={`text-sm font-medium ${
+                      apiAgent === agent.id ? "text-blue-600" : "text-gray-800"
+                    } dark:text-gray-300`}
+                  >
+                    {agent.name}
+                  </p>
+                </div>
+              )
+            )}
+          </div>
+          {apiAgent && (
+            <div className="mt-4 flex justify-end">
+              <button
+                type="button"
+                className="rounded bg-blue-500 px-4 py-2 text-sm text-white transition-all hover:bg-blue-400 dark:hover:bg-blue-600"
+                onClick={() => nextStep()}
+              >
+                Siguiente
+              </button>
+            </div>
+          )}
         </div>
-      </form>
+      )}
     </div>
   )
 }
