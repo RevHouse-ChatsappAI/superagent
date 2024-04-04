10cde0e8a524ebf228d9a4e0f3ba8d35f5220835
GonzaloArray <gonzalo.arrayaran@gmail.com>
2024-04-04 06:32:24 -0300

    Feat/ navbar

---
+++ libs/ui/components/NavBar.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import React from "react"
+import NextLink from "next/link"
+import { FiSend } from "react-icons/fi"
+import { PiHeadphones } from "react-icons/pi"
+import { RiShareBoxFill } from "react-icons/ri"
+
+import { DarkMode } from "./icons/DarkMode"
+
+export const NavBar = () => {
+  return (
+    <nav className="flex justify-end gap-10 px-6 py-3">
+      <NextLink href="/asdfsadf" className="flex items-center gap-2">
+        <PiHeadphones className="text-xl" />
+        <span>Centro de ayuda</span>
+      </NextLink>
+      <NextLink href="/asdfsadf" className="flex items-center gap-2">
+        <FiSend className="text-lg" />
+        <span>CRM</span>
+        <RiShareBoxFill className="text-lg" />
+      </NextLink>
+      <button className="rounded-lg p-2 transition-all hover:bg-slate-100/30">
+        <DarkMode />
+      </button>
+    </nav>
+  )
+}
