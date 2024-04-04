37c531dd3fae271f21c48b908c76859c766be486
GonzaloArray <gonzalo.arrayaran@gmail.com>
2024-04-04 06:32:56 -0300

    Feat/ new design

--- libs/ui/app/container.tsx
+++ libs/ui/app/container.tsx
@@ -1,3 +1,4 @@
+import { NavBar } from "@/components/NavBar"
 import Sidebar from "@/components/sidebar"
 
 import BillingModal from "./billing-modal"
@@ -9,14 +10,17 @@ interface RootLayoutProps {
 
 export default function RootLayout({ children, profile }: RootLayoutProps) {
   return (
-    <section className="flex h-screen">
+    <section className="flex h-screen flex-col">
       {/*
       {process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY && (
         <BillingModal profile={profile} />
       )}
       */}
-      <Sidebar />
-      <div className="flex-1 overflow-auto">{children}</div>
+      <NavBar />
+      <div className="flex">
+        <Sidebar />
+        <div className="ml-16 flex-1 overflow-auto">{children}</div>
+      </div>
     </section>
   )
 }
