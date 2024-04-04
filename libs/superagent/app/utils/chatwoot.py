e079eac5e7fb2d4a5901a6612a70f31867355959
GonzaloArray <gonzalo.arrayaran@gmail.com>
2024-04-04 06:38:03 -0300

    Feat/ utils v2 superagent

--- libs/superagent/app/utils/chatwoot.py
+++ libs/superagent/app/utils/chatwoot.py
@@ -1,21 +1,29 @@
+from os import getenv
+
 import aiohttp
 from aiohttp import ClientSession
-
-from os import getenv
 
 CHATWOOT_API_URL = getenv("CHATWOOT") + "/accounts/"
 SUBSCRIPTION = getenv("SUBSCRIPTION")
 
 
-async def enviar_respuesta_chatwoot(conversation_id, respuesta, token, account_id, es_respuesta_de_bot=True):
+async def enviar_respuesta_chatwoot(
+    conversation_id, respuesta, token, account_id, es_respuesta_de_bot=True
+):
     str_account_id = str(account_id)
     str_conversation_id = str(conversation_id)
-    url = f"{CHATWOOT_API_URL}{str_account_id}/conversations/{str_conversation_id}/messages"
+    url = (
+        f"{CHATWOOT_API_URL}{str_account_id}/conversations/"
+        f"{str_conversation_id}/messages"
+    )
 
-    headers = {"Content-Type": "application/json", "api_access_token": token, 'Ocp-Apim-Subscription-Key': SUBSCRIPTION}
+    headers = {
+        "Content-Type": "application/json",
+        "api_access_token": token,
+        "Ocp-Apim-Subscription-Key": SUBSCRIPTION,
+    }
 
     data = {"content": respuesta, "message_type": "outgoing", "private": False}
-    # No incluir el sender si la respuesta es del bot
     if not es_respuesta_de_bot:
         data["sender"] = {"name": "John", "available_name": "John"}
 
@@ -23,7 +31,42 @@ async def enviar_respuesta_chatwoot(conversation_id, respuesta, token, account_i
         async with session.post(url, headers=headers, json=data) as response:
             if response.status != 200:
                 response_body = await response.text()
-                print(f"Response body: {response_body}")
-                error_message = f"Failed to send message, status: {response.status}, response: {response_body}"
+                error_message = (
+                    f"Failed to send message, status: {response.status}, "
+                    f"response: {response_body}"
+                )
                 return {"error": error_message}
             return {"status": response.status, "data": "Message sent successfully"}
+
+
+async def chatwoot_human_handoff(conversation_id, token, account_id):
+    str_account_id = str(account_id)
+    str_conversation_id = str(conversation_id)
+
+    url = (
+        f"{CHATWOOT_API_URL}{str_account_id}/conversations/"
+        f"{str_conversation_id}/toggle_status"
+    )
+
+    headers = {
+        "Content-Type": "application/json",
+        "api_access_token": token,
+        "Ocp-Apim-Subscription-Key": SUBSCRIPTION,
+    }
+
+    data = {"status": "open"}
+
+    async with aiohttp.ClientSession() as session:
+        try:
+            async with session.post(url, headers=headers, json=data) as response:
+                if response.status != 200:
+                    print(f"Failed to add labels, status: {response.status}")
+                    raise aiohttp.web.HTTPException(
+                        reason=f"Network response was not ok. Status: {response.status}"
+                    )
+
+                response_data = await response.json()
+                print("Label added successfully:", response_data)
+        except Exception as error:
+            print("Error adding label:", error)
+            raise
