771bf6a07f0b931cd6cf8dd376d5f0af3994aa40
GonzaloArray <gonzalo.arrayaran@gmail.com>
2024-05-14 15:09:04 -0300

    Added integrations object

--- libs/ui/config/integration.ts
+++ libs/ui/config/integration.ts
@@ -27,5 +27,57 @@ export const integrationsConfig = [
         placeholder: "Ingrese su clave",
       },
     ],
+    subMetaData: [
+      {
+        key: "name",
+        type: "input",
+        label: "Nombre del usuario",
+        required: true,
+        placeholder: "Ej: Bot Liftel",
+      },
+      {
+        key: "email",
+        type: "input",
+        label: "Correo electronico",
+        required: true,
+        placeholder: "Ej: bot@chatsappai.com",
+      },
+      {
+        key: "password",
+        type: "password",
+        label: "Contraseña",
+        required: true,
+        placeholder: "Ingrese su contraseña",
+      },
+      {
+        type: "separator",
+      },
+      {
+        key: "account_name",
+        type: "input",
+        label: "Nombre de cuenta",
+        required: true,
+        placeholder: "Ej: Empresa Chatsapp",
+        helpText: "En esta sección se configuran la cuenta dentro de chatwoot",
+      },
+      {
+        type: "separator",
+      },
+      {
+        key: "agent_name",
+        type: "input",
+        label: "Nombre del agente",
+        required: true,
+        agent: true,
+        placeholder: "Ej: agente comercio",
+      },
+      {
+        key: "agent_description",
+        type: "input",
+        label: "Desscripción del agente",
+        required: true,
+        placeholder: "Ingrese una breve descripción",
+      },
+    ],
   },
 ]
