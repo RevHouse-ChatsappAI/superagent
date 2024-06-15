b0f0775ab0428d73e602a29ab6cd9b3ea473db8a
GonzaloArray <gonzalo.arrayaran@gmail.com>
2024-06-15 15:59:36 -0300

    Added conditional rendering

--- libs/ui/components/ui/multi-select.tsx
+++ libs/ui/components/ui/multi-select.tsx
@@ -53,7 +53,7 @@ export const MultiSelect = ({
     []
   )
 
-  const selectables = data.filter((obj) => !selected.includes(obj))
+  const selectables = data?.filter((obj) => !selected.includes(obj))
 
   React.useEffect(() => {
     onChange?.(selected)
@@ -65,14 +65,14 @@ export const MultiSelect = ({
       onKeyDown={handleKeyDown}
       className="overflow-visible bg-transparent"
     >
-      <div className="group rounded-md border border-input px-3 py-2 text-sm ring-offset-background focus-within:ring-2 focus-within:ring-ring focus-within:ring-offset-2">
+      <div className="border-input ring-offset-background focus-within:ring-ring group rounded-md border px-3 py-2 text-sm focus-within:ring-2 focus-within:ring-offset-2">
         <div className="flex flex-wrap gap-1">
           {selected.map((obj) => {
             return (
               <Badge key={obj.value} variant="secondary">
                 {obj.label}
                 <button
-                  className="ml-1 rounded-full outline-none ring-offset-background focus:ring-2 focus:ring-ring focus:ring-offset-2"
+                  className="ring-offset-background focus:ring-ring ml-1 rounded-full outline-none focus:ring-2 focus:ring-offset-2"
                   onKeyDown={(e) => {
                     if (e.key === "Enter") {
                       handleUnselect(obj)
@@ -84,7 +84,7 @@ export const MultiSelect = ({
                   }}
                   onClick={() => handleUnselect(obj)}
                 >
-                  <X className="h-3 w-3 text-muted-foreground hover:text-foreground" />
+                  <X className="text-muted-foreground hover:text-foreground h-3 w-3" />
                 </button>
               </Badge>
             )
@@ -97,13 +97,13 @@ export const MultiSelect = ({
             onBlur={() => setOpen(false)}
             onFocus={() => setOpen(true)}
             placeholder={placeholder}
-            className="ml-2 flex-1 bg-transparent outline-none placeholder:text-muted-foreground"
+            className="placeholder:text-muted-foreground ml-2 flex-1 bg-transparent outline-none"
           />
         </div>
       </div>
       <div className="relative mt-2">
         {open && selectables.length > 0 ? (
-          <div className="absolute top-0 z-10 w-full rounded-md border bg-popover text-popover-foreground shadow-md outline-none animate-in">
+          <div className="bg-popover text-popover-foreground animate-in absolute top-0 z-10 w-full rounded-md border shadow-md outline-none">
             <CommandGroup className="h-full overflow-auto">
               {selectables.map((obj) => {
                 return (
